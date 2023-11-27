3e2ee124e3181f89b14c3072fcbbaef82720363c
luiz fernando sales <lnandofonseca@gmail.com>
2023-11-27 17:14:37 -0300

    before-heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@ require "faker"
     password: Faker::Internet.password,
   )
 end
+
+10.times do
+  Firm.create!(
+    name: Faker::Company.name,
+    sector: Faker::Company.department,
+    description: Faker::Company.brazilian_company_number
+  )
+end
