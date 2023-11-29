8d99d906633d59bd655933968a0531e400f7b317
Luis Borges <lfelipepeixoto92@gmail.com>
2023-11-29 12:27:05 -0300

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "pages#home"
-  resources :company
+
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      get 'skills', to: 'users#skills'
+      get 'favorite_jobs', to: 'users#favorite_jobs'
+    end
+  end
+
+  resources :companies
 end
