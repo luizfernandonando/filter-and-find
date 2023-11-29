467d26ccbf664af96b7d13b3605fd45dd4763c1a
yan monteiro <yanlinhares9@hotmail.com>
2023-11-29 14:16:01 -0300

    jobs-filter

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
   resources :companies, only: %i[show index]
+  resources :jobs, only: %i[show index]
   resources :users, only: [:show, :edit, :update] do
     member do
       get 'skills', to: 'users#skills'
       get 'favorite_jobs', to: 'users#favorite_jobs'
     end
+  end
 end
