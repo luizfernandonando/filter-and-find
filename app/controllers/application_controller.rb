be6683b214eee5f3f5478905659c686737744045
yan monteiro <yanlinhares9@hotmail.com>
2023-12-04 14:04:36 -0300

    cloudinary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name photo])
   end
 end
