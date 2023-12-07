ec07be66cd4a603ef217d6b4fa95dabec183d1c3
yan monteiro <yanlinhares9@hotmail.com>
2023-12-07 14:54:09 -0300

    seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,17 @@
 # app/models/user.rb
 class User < ApplicationRecord
   has_one_attached :photo
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  has_many :reviews
-  has_many :user_skills
+  validates :email, :password, presence: true
+
+  has_many :reviews, dependent: :destroy
+  has_many :user_skills, dependent: :destroy
   has_many :skills, through: :user_skills
-  has_many :matches
+  has_many :matches, dependent: :destroy
+
   has_many :favorite_jobs, through: :matches, source: :job
 
-  validates :email, :password, presence: true
   validates :full_name, presence: true
 end
